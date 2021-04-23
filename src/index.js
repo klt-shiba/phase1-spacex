984eec61c3fea18ea6beaaa3b0aac13306483776
Colin Tseng <klt.aus@yahoo.com>
2021-04-24 00:05:29 +1000

    add hover effects and add new function for adding payload

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     let value = searchField.value.toLowerCase();
     console.log(value)
+
     let configurationObject = {
       method: "POST",
       headers: {
@@ -41,6 +42,8 @@ window.addEventListener('DOMContentLoaded', event => {
         console.log(object)
         renderSearch(object)
 
+
+
       })
       .catch(function (error) {
         console.log("Not working", error);
@@ -48,43 +51,68 @@ window.addEventListener('DOMContentLoaded', event => {
   });
 
 
-  let renderSearch = ((object) => {
-
-    let sectionContainer = document.getElementById('search-results')
-    let imgList = sectionContainer.querySelector('.img-container')
-    let launches = object.docs
-
-    // Check if there are launches 
-    imgList.innerHTML = "";
+  // Fetch Payload of ID
+  let fetchPayload = ((uid) => {
+    return fetch(`https://api.spacexdata.com/v4/payloads/${uid}`)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (object) {
+       let payLoad = object  
+       return payLoad
+      })
+      .then(function (payLoad) {
+        return payLoad
+      })
+      .catch(function (error) {
+          console.log(error)
+      })
     
-    if (launches.length > 0) {
-
-      sectionContainer.classList.remove("hidden");
-
-      for (launch of launches) {
-
-        let imgPath = shuffle(launch.links.flickr.original)
-        let cloneList = listTemplate.cloneNode(true)
-        cloneList.classList.remove('hidden')
-        imgList.append(cloneList)
-        let img = cloneList.querySelector('.img-tiles')
-        let heading = cloneList.querySelector('.list-item-heading')
-        let body = cloneList.querySelector('.list-item-description')
-        let button = cloneList.querySelector('button')
-        img.src = imgPath
-        heading.innerText = `${launch.name}`
-        let launchDetails = launch
-
-        button.addEventListener('click', function (e) {
-          console.log(launchDetails)
-          renderInfoPage(launchDetails)
-        })
-      }
-
-    } else {
-      console.log("empty")
-    }
   })
+  
+
+
+
+
+
+let renderSearch = ((object) => {
+
+  let sectionContainer = document.getElementById('search-results')
+  let imgList = sectionContainer.querySelector('.img-container')
+  let launches = object.docs
+
+  // Check if there are launches 
+  imgList.innerHTML = "";
+
+  if (launches.length > 0) {
+
+    sectionContainer.classList.remove("hidden");
+
+    for (launch of launches) {
+
+      let imgPath = shuffle(launch.links.flickr.original)
+      let cloneList = listTemplate.cloneNode(true)
+      cloneList.classList.remove('hidden')
+      imgList.append(cloneList)
+      let img = cloneList.querySelector('.img-tiles')
+      let heading = cloneList.querySelector('.list-item-heading')
+      let body = cloneList.querySelector('.list-item-description')
+      let button = cloneList.querySelector('button')
+      img.src = imgPath
+      heading.innerText = `${launch.name}`
+      let launchDetails = launch
+
+      button.addEventListener('click', function (e) {
+        console.log(launchDetails)
+        togglePendingState()
+        renderInfoPage(launchDetails)
+      })
+    }
+
+  } else {
+    console.log("empty")
+  }
+})
 // let loadRandomLaunches = () => {
 
 //   let url = `https://api.spacexdata.com/v4/launches/past`;
@@ -166,7 +194,7 @@ let loadLaunchList = () => {
       })
 
       // Shuffle launches so list is random each time
-      let shuffleLaunches = shuffle(launches).slice(0, 6)
+      let shuffleLaunches = shuffle(launches).slice(0, 24)
 
       // Render each list element with launch data
       for (launch of shuffleLaunches) {
@@ -178,12 +206,23 @@ let loadLaunchList = () => {
         let heading = cloneList.querySelector('.list-item-heading')
         let body = cloneList.querySelector('.list-item-description')
         let button = cloneList.querySelector('button')
+
+
+        if (launch.success === true) {
+          button.classList.add("success")
+        } else {
+          button.classList.add("failure")
+        }
+
         img.src = imgPath
         heading.innerText = `${launch.name}`
         // body.innerText = `${launch.details}`;
         let launchDetails = launch
+
         button.addEventListener('click', function (e) {
+          let payload = fetchPayload(launchDetails.payloads[0])
           console.log(launchDetails)
+          togglePendingState()
           renderInfoPage(launchDetails)
         })
       }
@@ -197,48 +236,67 @@ let readableDates = date => {
   let newDate = date.slice(0, 10)
   return newDate
 }
-let renderInfoPage = info => {
+
+// Function to toggle the pending state / loading bar
+let togglePendingState = (() => {
+  const slider = document.getElementById('slider')
+  slider.classList.remove("hidden")
+  setTimeout(function () {
+    slider.classList.add("hidden");
+  }, 3000)
+})
+
+let renderInfoPage = ((info, payload) => {
 
   // Clear search bar 
   searchField.value = "";
+
   // Hide/Show the correct section
   for (let section of sections) {
     if (section.id === 'details-page') {
-      section.classList.remove('hidden')
+      setTimeout(function () {
+        section.classList.remove('hidden')
+      }, 3000);
     } else {
       section.classList.add('hidden')
     }
   }
-
+  
   //Check if launch was successful or failure = 
   const imgList = detailsPage.querySelector('.img-container')
   const heading = detailsPage.querySelector('h2')
   const summary = detailsPage.querySelector('p')
   const date = detailsPage.querySelector('.date')
+  const flightNumber = detailsPage.querySelector('.status-indicator.number')
   const statusIndicator = detailsPage.querySelector('.status-indicator')
   const informationCard = detailsPage.querySelector('.information-card')
   const articleLink = informationCard.querySelector(".article-link a")
   const pressKitLInk = informationCard.querySelector(".presskit-link a")
 
-  // Populate Heading and Summary
-  heading.innerText = `${info.name}`
-  summary.innerText = `${info.details}`
-  date.innerHTML = readableDates(`${info.date_local}`)
-
-  // Populate Links
-  articleLink.href = info.links.article
-  pressKitLInk.href = info.links.presskit
-
   // Check if launch was successful or failure and apply correct styling
   if (info.success === true) {
     statusIndicator.innerHTML = "Success"
     statusIndicator.classList.remove("failure")
+    flightNumber.classList.remove("failure")
     statusIndicator.classList.add("success")
+    flightNumber.classList.add("success")
   } else {
     statusIndicator.innerHTML = "Failure"
+    flightNumber.classList.remove("success")
     statusIndicator.classList.remove("success")
     statusIndicator.classList.add("failure")
+    flightNumber.classList.add("failure")
   }
+
+  // Populate Heading and Summary
+  heading.innerText = `${info.name}`
+  summary.innerText = `${info.details}`
+  date.innerHTML = readableDates(`${info.date_local}`)
+  flightNumber.innerHTML = `Flight #${info.flight_number}`
+
+  // Populate Links
+  articleLink.href = info.links.article
+  pressKitLInk.href = info.links.presskit
 
   // Photos Array
   let photos = info.links.flickr.original
@@ -250,7 +308,7 @@ let renderInfoPage = info => {
     imgList.append(img)
   }
 
-}
+})
 loadLaunchList()
 clearPage()
 loadSection()
