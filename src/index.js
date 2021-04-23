ba28f3419b01e8f0db5fe6424995c91ad6294598
Colin Tseng <klt.aus@yahoo.com>
2021-04-23 20:08:55 +1000

    Add more detail to the details page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  let searchField = document.getElementById("search-photos");
-  let submitBtn = document.getElementById("submitBtn");
-  let userList = document.getElementById("img-container");
-  let sectionCrew = document.getElementById("space-crew");
-  let sectionCta = document.querySelectorAll(".section-cta");
-  let listTemplate = document.querySelector(".clone-list");
-  let launchListSection = document.getElementById("launch-list");
-  let sections = document.querySelectorAll("section");
-  let detailsPage = document.getElementById("details-page");
+window.addEventListener('DOMContentLoaded', event => {
+  let searchField = document.getElementById('search-photos')
+  let submitBtn = document.getElementById('submitBtn')
+  let userList = document.getElementById('img-container')
+  let sectionCrew = document.getElementById('space-crew')
+  let sectionCta = document.querySelectorAll('.section-cta')
+  let listTemplate = document.querySelector('.clone-list')
+  let launchListSection = document.getElementById('launch-list')
+  let sections = document.querySelectorAll('section')
+  let detailsPage = document.getElementById('details-page')
   // submitBtn.addEventListener("click", function (e) {
   //   e.preventDefault();
   //   let value = searchField.value.toLowerCase();
@@ -73,97 +73,135 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
+      const j = Math.floor(Math.random() * (i + 1))
+        ;[a[i], a[j]] = [a[j], a[i]]
     }
-    return a;
+    return a
   }
-  let loadSection = (e) => {
-    let btn = e.target;
-    console.log(btn.id);
-    if (btn.id === "show-more-events") {
-      launchListSection.classList.remove("hidden");
+  let loadSection = e => {
+    let btn = e.target
+    console.log(btn.id)
+    if (btn.id === 'show-more-events') {
+      launchListSection.classList.remove('hidden')
     } else {
     }
-  };
+  }
 
   let clearPage = () => {
     for (let ctas of sectionCta) {
-      ctas.addEventListener("click", (e) => {
-        console.log(e.target);
+      ctas.addEventListener('click', e => {
+        console.log(e.target)
         for (let section of sections) {
-          section.classList.add("hidden");
+          section.classList.add('hidden')
         }
         setTimeout(function () {
-          loadSection(e);
-        }, 3000);
-      });
+          loadSection(e)
+        }, 3000)
+      })
     }
-  };
+  }
   // Render List of Launches
   let loadLaunchList = () => {
-    let url = `https://api.spacexdata.com/v4/launches/past`;
+    let url = `https://api.spacexdata.com/v4/launches/past`
     return fetch(url)
       .then(function (response) {
-        return response.json();
+        return response.json()
       })
       .then(function (object) {
-        let sectionContainer = document.getElementById("launch-list");
-        let imgList = sectionContainer.querySelector(".img-container");
+        let sectionContainer = document.getElementById('launch-list')
+        let imgList = sectionContainer.querySelector('.img-container')
 
         //Check if all objects have img srcs
-        let launches = object.filter((el) => {
-          return el.links.flickr.original.length !== 0;
-        });
-        let shuffleLaunches = shuffle(launches)
+        let launches = object.filter(el => {
+          return el.links.flickr.original.length !== 0
+        })
+
+        // Shuffle launches so list is random each time
+        let shuffleLaunches = shuffle(launches).slice(0, 6)
+
         // Render each list element with launch data
         for (launch of shuffleLaunches) {
-          let imgPath = shuffle(launch.links.flickr.original);
-          let cloneList = listTemplate.cloneNode(true);
-          cloneList.classList.remove("hidden");
-          imgList.append(cloneList);
-          let img = cloneList.querySelector(".img-tiles");
-          let heading = cloneList.querySelector(".list-item-heading");
-          let body = cloneList.querySelector(".list-item-description");
-          let button = cloneList.querySelector("button");
-          img.src = imgPath;
-          heading.innerText = `${launch.name}`;
+          let imgPath = shuffle(launch.links.flickr.original)
+          let cloneList = listTemplate.cloneNode(true)
+          cloneList.classList.remove('hidden')
+          imgList.append(cloneList)
+          let img = cloneList.querySelector('.img-tiles')
+          let heading = cloneList.querySelector('.list-item-heading')
+          let body = cloneList.querySelector('.list-item-description')
+          let button = cloneList.querySelector('button')
+          img.src = imgPath
+          heading.innerText = `${launch.name}`
           // body.innerText = `${launch.details}`;
           let launchDetails = launch
-          button.addEventListener("click", function(e) {
+          button.addEventListener('click', function (e) {
             console.log(launchDetails)
             renderInfoPage(launchDetails)
-          });
+          })
         }
       })
       .catch(function (error) {
-        console.log("Not working", error);
-      });
-  };
-  let renderInfoPage = (info) => {
+        console.log('Not working', error)
+      })
+  }
+  let readableDates = date => {
+    // US format
+    let newDate = date.slice(0, 10)
+    return newDate
+  }
+  let renderInfoPage = info => {
+
+    // Hide/Show the correct section
     for (let section of sections) {
-      if (section.id === "details-page") {
-        section.classList.remove("hidden");
+      if (section.id === 'details-page') {
+        section.classList.remove('hidden')
       } else {
-        section.classList.add("hidden");
+        section.classList.add('hidden')
       }
     }
 
-    let imgList = detailsPage.querySelector(".img-container");
-    let heading = detailsPage.querySelector("h2");
-    let summary = detailsPage.querySelector("p");
-    heading.innerText = `${info.name}`;
-    summary.innerText = `${info.details}`;
+    //Check if launch was successful or failure = 
+    const imgList = detailsPage.querySelector('.img-container')
+    const heading = detailsPage.querySelector('h2')
+    const summary = detailsPage.querySelector('p')
+    const date = detailsPage.querySelector('.date')
+    const statusIndicator = detailsPage.querySelector('.status-indicator')
+    const informationCard = detailsPage.querySelector('.information-card')
+    const articleLink = informationCard.querySelector(".article-link a")
+    const pressKitLInk = informationCard.querySelector(".presskit-link a")
+
+    // Populate Heading and Summary
+    heading.innerText = `${info.name}`
+    summary.innerText = `${info.details}`
+    date.innerHTML = readableDates(`${info.date_local}`)
+    
+    // Populate Links
+    articleLink.href = info.links.article
+    pressKitLInk.href = info.links.presskit
+
+    // Check if launch was successful or failure and apply correct styling
+    if (info.success === true) {
+      statusIndicator.innerHTML = "Success"
+      statusIndicator.classList.remove("failure")
+      statusIndicator.classList.add("success")
+    } else {
+      statusIndicator.innerHTML = "Failure"
+      statusIndicator.classList.remove("success")
+      statusIndicator.classList.add("failure")
+    }
+    
+    // Photos Array
     let photos = info.links.flickr.original
+
+    // Populate photos 
     for (let photo of photos) {
-      let img = document.createElement("img") 
+      let img = document.createElement('img')
       img.src = photo
       imgList.append(img)
     }
-  };
-
-  loadLaunchList();
-  clearPage();
-  loadSection();
-  loadRandomLaunches();
-});
+    
+  }
+  loadLaunchList()
+  clearPage()
+  loadSection()
+  loadRandomLaunches()
+})
